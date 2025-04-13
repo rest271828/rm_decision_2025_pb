7ed15478c09586273e44f532671a1e8720bf0fea
heihei611 <20233473@stu.edu.cn>
2025-04-13 12:56:59 +0800

    changed the node name of rotator

--- src/Action/rotator/src/rotator.cpp
+++ src/Action/rotator/src/rotator.cpp
@@ -1,6 +1,6 @@
 #include "rotator/rotator.hpp"
 
-Rotator::Rotator(const rclcpp::NodeOptions& options) : Node("navigator", options) {
+Rotator::Rotator(const rclcpp::NodeOptions& options) : Node("rotator", options) {
     angular_cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
     angular_velocity_sub_ = this->create_subscription<std_msgs::msg::Float32>(
         "to_rotator", 10, std::bind(&Rotator::angular_velocity_callback, this, std::placeholders::_1));
