fe1bfe9f0ecff08f6c7a83f3f5523a8930c7539c
heihei611 <20233473@stu.edu.cn>
2025-04-25 00:56:51 +0800

    sqd did

---
+++ src/Decision/decision_test_gamma/include/decision_test_gamma/decision_test_gamma.hpp
@@ -0,0 +1,15 @@
+#include "decision_beta/decision_beta.hpp"
+
+class DecisionTestGamma : public RMDecision::DecisionBeta {
+public:
+    explicit DecisionTestGamma(const rclcpp::NodeOptions& options);
+
+private:
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+
+    void pose_sub_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+
+    void route_a() const;
+
+    void test_response(const std::string& instruction, const std::vector<float>& args) const override;
+};
